7b6b15a85e4f961ce1b28c83dde320e7ea6085b5
Tasnimsomo <tasnasomo@gmail.com>
2025-01-22 12:09:22 +0400

    sql model for to-do-list application

--- models.py
+++ models.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Task(db.Model):
+    __tablename__ = 'tasks'
     id = db.Column(db.integer, primary_key=True)
     title = db.Colum(db.String(100), nullable=False)
-    completed = db.Column(db.Boolean, default=False)
+    status = db.Column(db.Boolean, default=False)
     
