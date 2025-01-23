8e3d3ea52ca6499349505ab5c344bbd6ee5db239
Tasnimsomo <tasnasomo@gmail.com>
2025-01-23 12:17:15 -0600

    connection to database

---
+++ database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.env')
+SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
