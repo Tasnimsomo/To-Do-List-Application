58200903e166d3856969603c428f78e2978e1059
Tasnimsomo <tasnasomo@gmail.com>
2025-01-31 11:25:53 -0600

    model for database

--- models.py
+++ models.py
@@ -2,13 +2,15 @@ from flask_sqlalchemy import Column, Integer, String, Boolean
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 
+# this creates a base class that allows sqlalchemy to automatically map classes to tables
 Base = declarative_base()
 
+# class Task inherits from Base to create a table named tasks
 class Task(Base):
     __tablename__ = 'tasks'
     id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
-    status = Column(Boolean, default=False)
-    created_at = Column(DateTime, default=datetime.now)
+    completed = Column(Boolean, default=False)
+    created_at = Column(datetime, default=datetime.now)
 
     
