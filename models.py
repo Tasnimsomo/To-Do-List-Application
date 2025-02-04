6678ef47229df74050fc358c198221787f886e84
Tasnimsomo <tasnasomo@gmail.com>
2025-02-04 11:55:08 -0600

    model for tables

--- models.py
+++ models.py
@@ -10,6 +10,7 @@ class Task(Base):
     __tablename__ = 'tasks'
     id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
+    description = Column(String(500), nullable=True)
     completed = Column(Boolean, default=False)
     created_at = Column(datetime, default=datetime.now)
 
