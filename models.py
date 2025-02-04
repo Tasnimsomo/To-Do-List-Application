cf4138c35a58136c6aaab67d963998cb4a7f6a35
Tasnimsomo <tasnasomo@gmail.com>
2025-02-04 12:21:26 -0600

    model for database table

--- models.py
+++ models.py
@@ -1,17 +1,18 @@
-from flask_sqlalchemy import Column, Integer, String, Boolean
-from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
 # this creates a base class that allows sqlalchemy to automatically map classes to tables
 Base = declarative_base()
+db = SQLAlchemy()
 
 # class Task inherits from Base to create a table named tasks
 class Task(Base):
     __tablename__ = 'tasks'
-    id = Column(Integer, primary_key=True)
-    title = Column(String(100), nullable=False)
-    description = Column(String(500), nullable=True)
-    completed = Column(Boolean, default=False)
-    created_at = Column(datetime, default=datetime.now)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500), nullable=True)
+    completed = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
 
     
