c24e4ad8101b8d538e551ca2dd92d3065f342025
Tasnimsomo <tasnasomo@gmail.com>
2025-02-06 12:24:24 -0600

    main application entry point

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, json
 from models import Task
 from database import session
 from datetime import datetime
@@ -30,11 +30,10 @@ def add_task():
 @app.route('/view_tasks', methods=['GET'])
 def view_tasks():
     tasks = session.query(Task).all()
-
     task_list = [
         {
             "title": task.title,
-            "description": task.description,
+            "description": task.description if isinstance(task.description, dict) else json.loads(task.description),
             "completed": task.completed,
             "id": task.id,
             "created_at": task.created_at
@@ -58,6 +57,46 @@ def delete_task(task_id):
     session.commit()
     return jsonify({
         'message': f"Task {task.title} deleted successfully"
+    }), 200
+
+
+## update by id
+@app.route('/update_task/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = session.query(Task).filter_by(Task.id == task_id).first()
+    if not task:
+        return jsonify({
+            'message': "Task not found"
+        }), 404
+
+    data = request.get_json()
+    title = data.get('title')
+    description = data.get('description')
+    completed = data.get('completed')
+    if title:
+        task.title = title
+    if description:
+        task.description = description
+    if completed:
+        task.completed = completed
+    session.commit()
+    return jsonify({
+        'message': 'Task updated successfully'
+    }), 200
+    
+
+# toggle task by id
+@app.route('/toggle_task/<int:task_id>', methods=['PATCH'])
+def toggle_task(task_id):
+    task = session.query(Task).filter(Task.id == task_id).first()
+    if not task:
+        return jsonify({
+            'message': "Task not found"
+        }), 404
+    task.completed = not task.completed
+    session.commit()
+    return jsonify({
+        'message': f'Task {task.title} toggled successfully'
     }), 200
 
 
