6c7ed6f804aad5a4a4c153ac248a066599546aeb
Tasnimsomo <tasnasomo@gmail.com>
2025-02-06 12:24:33 -0600

    model for database

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
+from sqlalchemy.dialects.postgresql import JSONB
 
 # this creates a base class that allows sqlalchemy to automatically map classes to tables
 Base = declarative_base()
@@ -11,7 +12,7 @@ class Task(Base):
     __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(500))
+    description = db.Column(JSONB)
     completed = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
 
